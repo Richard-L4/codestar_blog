f5eb84271a91d84417dff3e4212d26236b957424
Richard-L4 <rwells945@gmail.com>
2025-03-27 12:50:37 +0000

    new posts added and home page created

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&8c7-m&=q450vb%y^l5-ngri))ka_077uh-g^&_866*hs^#yow'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
