081205e3fc78965f026931931f70b0c16dea2168
Richard-L4 <rwells945@gmail.com>
2025-04-28 18:51:48 +0100

    another way

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,17 @@
-from .models import Comment
 from django import forms
+from .models import Comment
+from allauth.account.forms import SignupForm
 
-
+# Form for blog comments
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+# Custom signup form for allauth (outside of CommentForm)
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Replace the help texts so there are no <ul> or <li> in them
+        self.fields['password1'].help_text = "Password must be at least 8 characters."
+        self.fields['password2'].help_text = "Enter the same password again to verify."
--- codestar/settings.py
+++ codestar/settings.py
@@ -137,6 +137,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+ACCOUNT_FORMS = {
+    'signup': 'blog.forms.CustomSignupForm',
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
