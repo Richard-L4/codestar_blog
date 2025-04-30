df16e05688a21302f355b926780cd7098c783e66
Richard-L4 <rwells945@gmail.com>
2025-04-30 10:32:53 +0100

    Update urls.py

--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 
+# URL configuration for the 'about' app
+
 urlpatterns = [
+    # Maps the root URL of this app to the about_me view
     path('', views.about_me, name='about'),
 ]
