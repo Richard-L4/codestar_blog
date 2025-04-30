4dc67a1abacf5479b6f8d180a5b549ffda0222a7
Richard-L4 <rwells945@gmail.com>
2025-04-30 14:16:57 +0100

    Update apps.py

--- blog/apps.py
+++ blog/apps.py
@@ -2,5 +2,15 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """
+    Configuration class for the 'blog' app.
+    This class defines the application-specific
+    settings for Django to recognize and manage the app properly.
+    """
+
+    # Specifies the type of auto-generated primary key field
+    # to use for models in this app
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Name of the application as used in INSTALLED_APPS
     name = 'blog'
